1ecf56f5264cd88176335da7ac5bb74109746aca
Chris Bennington <cbennington2011@gmail.com>
2017-04-22 11:17:17 +0100

    Added split string formatter

---
+++ Prospector.Presentation/Formatters/SplitStringFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prospector.Presentation.Formatters
+{
+    public static class SplitStringFormatter
+    {
+        public static String Convert(String input)
+        {
+            var splitInput = input.ToCharArray();
+            var result = "";
+
+            foreach (var item in splitInput)
+            {
+                if (Char.IsUpper(item))
+                {
+                    result = result + " " + item;
+                }
+                else
+                {
+                    result += item;
+                }
+            }
+
+            return result.Trim();
+        }
+    }
+}
---
+++ Prospector.UnitTests/Presentation/Formatters/SplitStringFormatterSpecs/SplitStringFormatterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+using Prospector.Presentation.Formatters;
+
+namespace Prospector.UnitTests.Presentation.Formatters.SplitStringFormatterSpecs
+{
+    public class WhenIConvertAString : TestBase<Object>
+    {
+        [Then]
+        public void TheResultIsCorrect()
+        {
+            Assert.That(SplitStringFormatter.Convert("ThisIsTheInputString"), Is.EqualTo("This Is The Input String"));
+        }
+    }
+}
