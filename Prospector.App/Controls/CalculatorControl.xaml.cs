7eb05b30cf00d978d7a502979aa704f4fa749b0a
Chris Bennington <cbennington2011@gmail.com>
2017-05-23 10:18:31 +0100

    Implemented holdings control

---
+++ Prospector.App/Controls/CalculatorControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Prospector.App.Controls
+{
+    
+    public partial class CalculatorControl : UserControl
+    {
+        public CalculatorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Prospector.App/Controls/HoldingsControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using Prospector.Presentation.Contracts;
+
+namespace Prospector.App.Controls
+{
+    public partial class HoldingsControl : UserControl
+    {
+        private readonly IHoldingViewModelBuilder _holdingViewModelBuilder;
+
+        public HoldingsControl(IHoldingViewModelBuilder holdingViewModelBuilder)
+        {
+            _holdingViewModelBuilder = holdingViewModelBuilder;
+
+            InitializeComponent();
+
+            PopulateTable();
+        }
+
+        internal void PopulateTable()
+        {
+            var holdingViewModels = _holdingViewModelBuilder.BuildViewModels();
+
+            HoldingsDataGrid.ItemsSource = holdingViewModels;
+        }
+    }
+}
